19e51c143a99e89fb91bafb6f9e12290be077e58
boykodmitryv <boykodmitryv@mail.ru>
2024-12-14 13:10:18 +0300

    commit-2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,7 +47,6 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    // const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
     const filteredUsers = users.myFriends.filter(c => c.address.city === 'Los Angeles');
     setCurrentUsers({ myFriends: filteredUsers });
   };
